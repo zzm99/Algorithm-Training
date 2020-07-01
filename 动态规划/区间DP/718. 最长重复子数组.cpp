8766731ede8102a328106d5d2b8d2fc46b894f8f
Jacky Min <43439842+zzm99@users.noreply.github.com>
2020-07-01 10:05:24 +0800

    71

---
+++ 动态规划/区间DP/718. 最长重复子数组.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findLength(vector<int>& A, vector<int>& B) {
+        int n = A.size(), m = B.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                dp[i][j] = A[i] == B[j] ? dp[i + 1][j + 1] + 1 : 0;
+                ans = max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
+};
