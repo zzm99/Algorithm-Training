701caf700bc688fff5a29db73ee65281f483dc9d
Jacky Min <43439842+zzm99@users.noreply.github.com>
2020-07-13 11:29:23 +0800

    0712

---
+++ 模拟/350. 两个数组的交集 II.cpp
@@ -0,0 +1,77 @@
+/*
+350. 两个数组的交集 II
+给定两个数组，编写一个函数来计算它们的交集。
+
+示例 1:
+
+输入: nums1 = [1,2,2,1], nums2 = [2,2]
+输出: [2,2]
+示例 2:
+
+输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出: [4,9]
+说明：
+
+输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+我们可以不考虑输出结果的顺序。
+进阶:
+
+如果给定的数组已经排好序呢？你将如何优化你的算法？
+如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+*/
+
+//为了降低空间复杂度，首先遍历较短的数组并在哈希表中记录每个数字以及对应出现的次数，然后遍历较长的数组得到交集。
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return intersect(nums2, nums1);
+        }
+        unordered_map <int, int> m;
+        for (int num : nums1) {
+            ++m[num];
+        }
+        vector<int> intersection;
+        for (int num : nums2) {
+            if (m.count(num)) {
+                intersection.push_back(num);
+                --m[num];
+                if (m[num] == 0) {
+                    m.erase(num);
+                }
+            }
+        }
+        return intersection;
+    }
+};
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int length1 = nums1.size(), length2 = nums2.size();
+        vector<int> intersection;
+        int index1 = 0, index2 = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                intersection.push_back(nums1[index1]);
+                index1++;
+                index2++;
+            }
+        }
+        return intersection;
+    }
+};
+
+/*
+如果 nums2
+的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中。那么就无法高效地对 nums2
+进行排序，因此推荐使用方法一而不是方法二。在方法一中，nums2 只关系到查询操作，因此每次读取 nums2 
+中的一部分数据，并进行处理即可。
+*/
